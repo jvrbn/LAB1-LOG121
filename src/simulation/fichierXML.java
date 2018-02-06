5383b2a8b2269ded2b3e07f47f929dfc7ec195fc
Javier Beltran <javier.beltran.1@ens.etsmtl.ca>
2018-02-05 21:05:07 -0500

    Classe fonctionnel afficahge usine sans deplacemen

---
+++ src/simulation/fichierXML.java
@@ -0,0 +1,17 @@
+package simulation;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+public class fichierXML {
+
+    
+
+}
+
+
